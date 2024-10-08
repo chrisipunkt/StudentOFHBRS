276608d50ef3d1346aa989a2f04e488ecfce5a3c
chris <christopher.immik@gmx.de>
2024-10-08 16:54:27 +0200

    Kommentar zum Cast in der Factory Klasse abgegeben.

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
@@ -8,7 +8,8 @@ public class Factory {
     public static Translator createGermanTranslator() {
         Translator translator = new GermanTranslator();  //Polymorphismus Objekt ist Typ Translator
 
-        ((GermanTranslator) translator).setDate("08-10-2024");
+        ((GermanTranslator) translator).setDate("08-10-2024"); //Cast, weil sonst kein Datum gesetzt werden darf.
+                                                               //Weil setDate() Methode in Interface fehlt
         return translator;  //alternativ return new GermanTranslator();
     }
 

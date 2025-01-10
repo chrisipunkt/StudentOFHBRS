592b2b31a4ee3c1bfb23f7e797c0248918863d8c
chris <christopher.immik@gmx.de>
2025-01-10 17:08:43 +0100

    U9 Commit BuchungDAO

---
+++ src/org/hbrs/se1/ws24/exercises/uebung9/BuchungDAO.java
@@ -0,0 +1,29 @@
+package org.hbrs.se1.ws24.exercises.uebung9;
+
+import java.util.List;
+
+public class BuchungDAO {
+
+    private List<Buchung> buchungen;
+
+    // Create a new booking
+    public void create(Buchung b) {
+        // Implementation here
+    }
+
+    // Read a booking by ID
+    public Buchung read(String id) {
+        // Implementation here
+        return null;
+    }
+
+    // Update an existing booking
+    public void update(Buchung b) {
+        // Implementation here
+    }
+
+    // Delete a booking by ID
+    public void delete(String id) {
+        // Implementation here
+    }
+}
